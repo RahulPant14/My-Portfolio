8cac73a195a5ad0b8494e9630879b4f143e11e11
RahulPant14 <166057013+RahulPant14@users.noreply.github.com>
2024-06-27 20:49:09 +0530

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,48 @@
+$(function(){
+
+
+
+    $(".wrap-7 .tabs a").click(function(e){
+        e.preventDefault();
+        $(".wrap-7 .tabs a").removeClass("active");
+        $(this).addClass("active");
+
+        if($(this).text()=="HTML CSS & JS"){
+            $(".wrap-7 .psd-html").fadeIn();
+            $(".wrap-7 .js").fadeOut(0);
+            $(".wrap-7 .bootstrap").fadeOut(0);
+            $(".wrap-7 .fetch").fadeOut(0);
+            $(".wrap-7 .sass").fadeOut(0);
+        }
+        else if($(this).text()=="HTML SASS & JS"){
+            $(".wrap-7 .psd-html").fadeOut(0);
+            $(".wrap-7 .bootstrap").fadeOut(0);
+            $(".wrap-7 .js").fadeOut(0);
+            $(".wrap-7 .fetch").fadeOut(0);
+            $(".wrap-7 .sass").fadeIn();
+         }
+        else if($(this).text()=="FETCH API"){ 
+            $(".wrap-7 .psd-html").fadeOut(0);
+            $(".wrap-7 .bootstrap").fadeOut(0);
+            $(".wrap-7 .sass").fadeOut(0);
+            $(".wrap-7 .js").fadeOut(0);
+            $(".wrap-7 .fetch").fadeIn();
+        }
+        else if($(this).text()=="JAVASCRIPT"){
+            $(".wrap-7 .psd-html").fadeOut(0);
+            $(".wrap-7 .sass").fadeOut(0);
+            $(".wrap-7 .fetch").fadeOut(0);
+            $(".wrap-7 .bootstrap").fadeOut(0);
+            $(".wrap-7 .js").fadeIn();
+         }
+         else if($(this).text()=="BOOTSTRAP"){
+            $(".wrap-7 .sass").fadeOut(0);
+            $(".wrap-7 .js").fadeOut(0);
+            $(".wrap-7 .psd-html").fadeOut(0);
+            $(".wrap-7 .fetch").fadeOut(0);
+            $(".wrap-7 .bootstrap").fadeIn();
+         }
+    });
+
+
+});
